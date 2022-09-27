c78070404a7c8ac4985bbaadb73c21c9067d7e23
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-09-27 15:50:58 +0100

    format course module

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,12 +1,12 @@
-import { Course } from './entities/course.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
-import { CourseService } from './course.service';
-import { CourseController } from './course.controller';
+import { Course } from "./entities/course.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from "@nestjs/common";
+import { CourseService } from "./course.service";
+import { CourseController } from "./course.controller";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Course])],
   controllers: [CourseController],
-  providers: [CourseService]
+  providers: [CourseService],
 })
 export class CourseModule {}
