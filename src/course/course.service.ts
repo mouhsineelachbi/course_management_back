0c1cda755b206f9d517e4e808b400de45f2ad3f1
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-09-27 15:50:48 +0100

    format course service

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,16 +1,16 @@
-import { Course } from './entities/course.entity';
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { CreateCourseDto } from './dto/create-course.dto';
-import { UpdateCourseDto } from './dto/update-course.dto';
-import { Repository } from 'typeorm';
+import { Course } from "./entities/course.entity";
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CreateCourseDto } from "./dto/create-course.dto";
+import { UpdateCourseDto } from "./dto/update-course.dto";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class CourseService {
   constructor(
     @InjectRepository(Course)
     private courseRepository: Repository<Course>
-  ){}
+  ) {}
   create(createCourseDto: CreateCourseDto) {
     return this.courseRepository.save(createCourseDto);
   }
@@ -19,24 +19,24 @@ export class CourseService {
     return this.courseRepository.find({
       relations: {
         students: true,
-        professor: true
-      }
+        professor: true,
+      },
     });
   }
 
   findOne(id: number) {
-    return this.courseRepository.findOne({where: {id: id}});
+    return this.courseRepository.findOne({ where: { id: id } });
   }
 
   findLastInserted() {
-    return this.courseRepository.findOne({order: {id: 'DESC'}});
+    return this.courseRepository.findOne({ order: { id: "DESC" } });
   }
 
   update(id: number, updateCourseDto: UpdateCourseDto) {
     return this.courseRepository.save(updateCourseDto);
   }
 
-  async removeMutlitple(courses: Course[]){
+  async removeMutlitple(courses: Course[]) {
     return this.courseRepository.remove(courses);
   }
 }
